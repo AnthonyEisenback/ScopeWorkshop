482342bc3eac3a47cda4b6288a2a24234670324f
AnthonyEisenback <anthonyeisenback@Interapts-MacBook-Pro-3.local>
2018-01-29 16:29:59 -0500

    Allowed methods to work with other methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,66 @@
 package com.company;
 
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 public class Main {
+
+    private String memberVariable = "I am a member variable, I have class level scope.";
+
+    private int memberInt = 10;
+    private int count = 0;
 
     public static void main(String[] args) {
 	// write your code here
+        Main main = new Main();
+        main.aMethod();
+        main.anotherMethod();
+        main.methodForLoopVariables();
+        main.memberVsLocalVariables();
+
+    }
+    private void aMethod() {
+        String localVariable = "I am a local variable, I can only be used in this method.";
+        System.out.println("Inside first method");
+        System.out.println(memberVariable);
+        System.out.println(localVariable);
+
+    }
+    private void anotherMethod (){
+        System.out.println("Inside first method");
+
+        System.out.println(memberVariable);
+
+    }
+
+    private void methodForLoopVariables () {
+        String localVariable = "Just another member variable outside our do while loop.";
+        System.out.println(localVariable);
+
+        boolean runDoLoop = true;
+        do {
+            String loopVariable = "I am a loop variable inside our do while loop";
+            System.out.println("Inside out do while loop: ");
+
+            System.out.println(localVariable);
+            runDoLoop = false;
+        }while (runDoLoop);
+
+        System.out.println("Inside third method, just outside do while loop: ");
+        System.out.println(localVariable);
+//        System.out.println();
+    }
+
+
+    private  void memberVsLocalVariables () {
+        int localInt = 7;
+
+        localInt *=2;
+        memberInt *=2;
+
+        if (count <=2) {
+            System.out.println("memberInt *=2 =" +memberInt);
+            System.out.println("localcount *= 2 =" + localInt);
+            count++;
+        }
     }
 }
